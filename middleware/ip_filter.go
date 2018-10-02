584f3b533c99b9254470956784c147d9a4092c42
Sam <sam42@outlook.com>
2018-10-02 23:51:07 +0800

    middleware

---
+++ middleware/ip_filter.go
@@ -0,0 +1,64 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/ltick/tick-routing"
+)
+
+type IPFilter struct {
+	whitelist []string
+	realIP    bool
+}
+
+// NewIPFilter creates middleware that intercepts the specified IP prefix.
+func NewIPFilter(whitelist []string, realIP bool) *IPFilter {
+	return &IPFilter{
+		whitelist: whitelist,
+		realIP:    realIP,
+	}
+}
+
+func (i *IPFilter) OnRequestStartup(c *routing.Context) error {
+	var forbidden bool
+	var match []string
+	var prefix []string
+	if len(i.whitelist) == 0 {
+		forbidden = true
+	} else {
+		for _, s := range i.whitelist {
+			if strings.HasSuffix(s, "*") {
+				prefix = append(prefix, s[:len(s)-1])
+			} else {
+				match = append(match, s)
+			}
+		}
+	}
+	if forbidden {
+		return routing.NewHTTPError(http.StatusForbidden)
+	}
+	var ip string
+	if i.realIP {
+		ip = c.GetClientRealIP()
+	} else {
+		ip = c.GetClientRemoteIP()
+	}
+	for _, ipMatch := range match {
+		if ipMatch == ip {
+			c.Next()
+			return nil
+		}
+	}
+	for _, ipPrefix := range prefix {
+		if strings.HasPrefix(ip, ipPrefix) {
+			c.Next()
+			return nil
+		}
+	}
+	return routing.NewHTTPError(http.StatusForbidden, "access not allow for ip: "+ip)
+}
+
+func (i *IPFilter) OnRequestShutdown(c *routing.Context) error {
+	return nil
+}
