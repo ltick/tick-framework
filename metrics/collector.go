97325f64f67fa453d7cb02f6cb6a450cee3719d9
Sam <sam42@outlook.com>
2018-12-05 12:13:56 +0800

    metrics

--- metrics/collector.go
+++ metrics/collector.go
@@ -8,60 +8,62 @@ import (
 )
 
 var (
-	errCollectorHasBeenRegistered = "metrics: collector has been registered"
-	errCollector                  = "metrics: register collector"
+	errCollectorHasRegistered  = "metrics: collector '%s' has registered"
+	errRegisterCollector       = "metrics: register collector '%s'"
+	errRegisterCustomCollector = "metrics: register custom collector '%s'"
 )
 
-var collectors map[string]prometheus.Collector = make(map[string]prometheus.Collector, 0)
-
-type Collector struct {
-	ClusterCollector *ClusterCollector
+type Metrics struct {
+	Collectors map[string]prometheus.Collector
 }
 
-func NewCollector(reg prometheus.Registerer, descs []*prometheus.Desc, zone string, hosts []string) *Collector {
-	c := &ClusterCollector{
-		descs: descs,
-		hosts: hosts,
-	}
-	cc := &Collector{
-		ClusterCollector: c,
-	}
-	prometheus.WrapRegistererWith(prometheus.Labels{"zone": zone}, reg).MustRegister(c)
-	reg.MustRegister(
-		prometheus.NewProcessCollector(prometheus.ProcessCollectorOpts{}),
-		prometheus.NewGoCollector(),
-	)
-	return cc
-}
-
-func (i *Collector) Prepare(ctx context.Context) (context.Context, error) {
+func (i *Metrics) Prepare(ctx context.Context) (context.Context, error) {
 	return ctx, nil
 }
-func (i *Collector) Initiate(ctx context.Context) (context.Context, error) {
+func (i *Metrics) Initiate(ctx context.Context) (context.Context, error) {
+	i.Collectors = make(map[string]prometheus.Collector, 0)
 	return ctx, nil
 }
-func (i *Collector) OnStartup(ctx context.Context) (context.Context, error) {
+func (i *Metrics) OnStartup(ctx context.Context) (context.Context, error) {
 	return ctx, nil
 }
-func (i *Collector) OnShutdown(ctx context.Context) (context.Context, error) {
+func (i *Metrics) OnShutdown(ctx context.Context) (context.Context, error) {
 	return ctx, nil
 }
 
-func (i *Collector) RegisterCollector(name string, cs prometheus.Collector) error {
-	if _, ok := collectors[name]; !ok {
-		collectors[name] = cs
+func (i *Metrics) RegisterCollector(name string, cs prometheus.Collector) error {
+	if _, ok := i.Collectors[name]; !ok {
+		i.Collectors[name] = cs
 		prometheus.MustRegister(cs)
 	} else {
-		return errors.New(errCollectorHasBeenRegistered)
+		return errors.Annotate(errors.Errorf(errCollectorHasRegistered, name), errRegisterCollector)
 	}
 	return nil
 }
-func (i *Collector) GetCollector(name string) (cs prometheus.Collector) {
-	if _, ok := collectors[name]; ok {
-		return collectors[name]
+func (i *Metrics) GetCollector(name string) (cs prometheus.Collector) {
+	if _, ok := i.Collectors[name]; ok {
+		return i.Collectors[name]
 	} else {
 		return nil
 	}
+}
+
+func (i *Metrics) RegisterCustomCollector(name string, reg prometheus.Registerer, descs []*prometheus.Desc, zone string, hosts []string) error {
+	if _, ok := i.Collectors[name]; !ok {
+		c := &ClusterCollector{
+			descs: descs,
+			hosts: hosts,
+		}
+		prometheus.WrapRegistererWith(prometheus.Labels{"zone": zone}, reg).MustRegister(c)
+		reg.MustRegister(
+			prometheus.NewProcessCollector(prometheus.ProcessCollectorOpts{}),
+			prometheus.NewGoCollector(),
+		)
+		i.Collectors[name] = c
+	} else {
+		return errors.Annotate(errors.Errorf(errCollectorHasRegistered, name), errRegisterCustomCollector)
+	}
+	return nil
 }
 
 type ClusterCollector struct {
