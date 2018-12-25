61141703fc49c400b9b1842ba33cf7d7393197e2
Sam <sam42@outlook.com>
2018-12-25 17:20:29 +0800

    rename metrics

--- server.go
+++ server.go
@@ -137,7 +137,7 @@ func ServerEnableMetricsHandlerDuration(handlers ...*prometheus.HistogramVec) Se
 			options.MetricsHandlerDuration = handlers[0]
 		} else {
 			options.MetricsHandlerDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-				Name:    "http_request_duration_seconds",
+				Name:    "http_server_requests_duration_seconds",
 				Help:    "A histogram of request latencies for requests.",
 				Buckets: []float64{.25, .5, 1, 2.5, 5, 10},
 			},
@@ -155,7 +155,7 @@ func ServerEnableMetricsHandlerResponseSize(handlers ...*prometheus.HistogramVec
 		} else {
 			options.MetricsHandlerResponseSize = prometheus.NewHistogramVec(
 				prometheus.HistogramOpts{
-					Name:    "http_response_size_bytes",
+					Name:    "http_server_requests_response_size_bytes",
 					Help:    "A histogram of response size for requests.",
 					Buckets: []float64{200, 500, 900, 1500},
 				},
@@ -173,7 +173,7 @@ func ServerEnableMetricsHandlerRequestSize(handlers ...*prometheus.HistogramVec)
 		} else {
 			options.MetricsHandlerRequestSize = prometheus.NewHistogramVec(
 				prometheus.HistogramOpts{
-					Name:    "http_request_size_bytes",
+					Name:    "http_server_requests_request_size_bytes",
 					Help:    "A histogram of request size for requests.",
 					Buckets: []float64{200, 500, 900, 1500},
 				},
